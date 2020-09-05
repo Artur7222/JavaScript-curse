f7b711a7a6189dbd1d5f7180b6bb3cb611214dc5
Ushakov Artur <ushakov.artur93@gmail.com>
2020-09-05 23:27:25 +0500

    закончил урок по Циклам

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 "use strict"
 
-if (4==9) {
-    console.log('OK');
-} else {
-    console.log('ebat ti matematik');
-}
+let num = 50;
 
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('To much');
-// } else {
-//     console.log('OK');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log('OK') : console.log('Error');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55) ;
 
-const num = 50;
-
-
-switch (num) {
-    case 49:
-        console.log('Error');
-        break;
-    case 100:
-        console.log('Error');
-        break;
-    case 50:
-        console.log("Oh my GOD, it's true!");
-        break;
-    default:
-        console.log("Otstan' Hvatit!");
-        break;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+         break;
     }
+    console.log(i);
+}
