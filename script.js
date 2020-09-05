6c6d69adaef6cf90561efa0218be9d9b1e10621b
Ushakov Artur <ushakov.artur93@gmail.com>
2020-09-05 22:51:37 +0500

    закончил урок по Условиям

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict"
+
+if (4==9) {
+    console.log('OK');
+} else {
+    console.log('ebat ti matematik');
+}
+
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('To much');
+// } else {
+//     console.log('OK');
+// }
+
+// (num === 50) ? console.log('OK') : console.log('Error');
+
+const num = 50;
+
+
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 50:
+        console.log("Oh my GOD, it's true!");
+        break;
+    default:
+        console.log("Otstan' Hvatit!");
+        break;
+    }
