0baace9a14b028bad4c2f380d6ee0121e9abf2e4
Ushakov Artur <ushakov.artur93@gmail.com>
2020-09-10 22:23:33 +0500

    изучение функций 3

--- script.js
+++ script.js
@@ -4,9 +4,37 @@ let num = 20;
 
 function showFirstMessage(text) {
     console.log(text);
-    num = 10;
+    let num = 10;
+    console.log(num);
 }
 
 showFirstMessage("Hello");
 console.log(num);
+
+// function calc(a, b) {
+//     return (a+b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(8, 10));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+
+const calc = (a, b) => return a + b;
+
+
+
 
