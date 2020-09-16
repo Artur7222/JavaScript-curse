dc949ea5d518d753fc0cc0fb2a2f3be72c2036b3
Ushakov Artur <ushakov.artur93@gmail.com>
2020-09-16 23:02:34 +0500

    Массивы и псевдомассивы

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 "use strict"
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("test")
-    }
-};
+const arr = [2, 13, 26, 8, 10];
+arr.sort(compareNum);
+console.log(arr);
 
-options.makeTest();
+function compareNum(a, b) {
+    return a-b;
+}
 
-const {border, bg} = options.colors;
+// // arr[99] = 0;
 
-console.log(border);
+// // console.log(arr.length);
 
-// console.log(Object.keys(options).length);
+// // console.log(arr);
 
-// console.log(options["colors"]['border']);
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`)
+// })
 
-// delete options.name;
 
-// console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key])==='object'){
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
-//             counter++;
-//         }
-//     }else {
-//     console.log(`Свойство ${key}, имеет значение ${options[key]}`);
-//     counter++;
-//     }
+// arr.pop();
+
+// arr.push(10);
+
+// console.log(arr);
+
+// // for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
 
-// console.log(counter);
+// for (let value of arr) {
+//     console.log(value);
+// }
 
-
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join(';'));
