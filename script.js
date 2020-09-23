7b037c79abe1222bc76eef116fd907165fd09e19
Ushakov Artur <ushakov.artur93@gmail.com>
2020-09-23 16:05:05 +0500

    Практическое занятие 4.Использование обьектов

--- script.js
+++ script.js
@@ -1,30 +1,81 @@
 "use strict"
 
-let str = "some";
-let str_obj = new String(str);
+const personalMovieDB = {
+    count: 0,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов', ''),
+                b = prompt('На сколько оцените его?', '');
+            if (a != null && b !=null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('Its done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert ('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+            alert ('Вы классический зритель');
+        } else if (personalMovieDB.count >=30) {
+            alert ('Вы киноман');
+        } else {
+            alert ('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        }else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+        //     let genres = prompt(`Ваш любимый жанр под номером ${i}`);
 
-console.log(typeof(str));
-console.log(typeof(str_obj));
+        //     if (genres === '' || genres == null) {
+        //         console.log('вы ввели некоректные данные или не ввели их вовсе');
+        //         i--;
+        //     }else {
+        //     personalMovieDB.genres[i-1] = genres; 
+        //     }
+        // }
 
-console.dir([1,2,3]); 
+        let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-const soldier = {
-    health: 400,
-    armor:100,
-    sayHello: function() {
-        console.log('Hello');    
+        if (genres === '' || genres == null) {
+                console.log('вы ввели некоректные данные или не ввели их вовсе');
+                i--;
+            }else {
+                personalMovieDB.genres = genres.split(',');
+                personalMovieDB.genres.sort(); 
+                }
+
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i+1} - это ${item}`);
+            });
+        }
     }
 };
 
-const john = Object.create(soldier);
-
-// const john = {
-//     health: 100
-// };
-
-// john.__proto__ = soldier;
-
-// Object.setPrototypeOf(john, soldier);
-
-// console.log(john.armor);
-john.sayHello();
