15ef8087d1cbb2ab51b0d1c82cfe348b2c365291
Ushakov Artur <ushakov.artur93@gmail.com>
2020-10-17 17:23:59 +0500

    Async,Defer, динамические скрипты

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// console.log(document.body);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
+const p = document.querySelectorAll('p');
+console.log(p);
 
-// console.log(document.body.lastChild);
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
 
-// console.log(document.querySelector('#current').parentElement);
-
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
-
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
+function loadScript (src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false; 
+    document.body.append(script);
 };
+
+loadScript('./test.js');
+loadScript('./some.js');
